9b76e43d66ef42b29b6654b59e94b77e3a71a709
Henrique Moraes <rique_moraes@hotmail.com>
2026-02-11 10:06:56 -0300

    Update login.spec - add loginPage

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,25 +1,40 @@
 import userData from '../fixtures/user-data.json'
+import LoginPage from '../pages/loginPage.js'
+
+const loginPage = new LoginPage()
 
 describe('Orange HRM Tests', () => {
 
   const selectorsList = {
-    usernameField: '[name="username"]',
-    passwordField: '[name="password"]',
-    loginButton: '.oxd-button',
-    dashboardGrid: '.orangehrm-dashboard-grid',
-    wrongCredentialAlert: '.oxd-alert'
+    dashboardGrid: '.orangehrm-dashboard-grid > :nth-child(1)',
+    myInfoButton: ':nth-child(6) > .oxd-main-menu-item',
+    firstNameField: '[name="firstName"]',
+    middleNameField: '[name="middleName"]',
+    lastNameField: '[name="lastName"]',
+    nationalityField: '.oxd-select-text',
+    nationalityDropdown: '.oxd-select-dropdown',
+    genericField: ''
   }
 
   
 
-  it('Login - Success', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userSucceess.username)
-    cy.get(selectorsList.passwordField).type(userData.userSucceess.password)
-    cy.get(selectorsList.loginButton).click()
+  it.only('Login - User Info Update - Success', () => {
+    loginPage.acessLoginPage()
+    loginPage.loginWithUser(userData.userSucceess.username, userData.userSucceess.password)
+    
+    
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboardGrid)
+    cy.get(selectorsList.myInfoButton).click()
+    cy.get(selectorsList.firstNameField, { timeout: 10000 }).clear().type('Henrique')
+    cy.get(selectorsList.middleNameField).clear().type('Galiano')
+    cy.get(selectorsList.lastNameField).clear().type('Moraes')
+    cy.get(selectorsList.nationalityField).eq(0).click()
+    cy.get(selectorsList.nationalityDropdown).contains('Brazilian').click()
+
   })
+
+
   it('Login - Fail', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userFail.username)
