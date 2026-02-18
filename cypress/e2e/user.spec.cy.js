6a9192ec20b278eab923a193f00e86fa4d09d068
Henrique Moraes <rique_moraes@hotmail.com>
2026-02-18 17:19:32 -0300

    add chanceJS

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -3,6 +3,9 @@ import LoginPage from '../pages/loginPage.js'
 import DashboardPage from '../pages/dashboardPage.js'
 import MyinfoPage from '../pages/myinfoPage.js'
 
+const Chance = require('chance');
+
+const chance = new Chance();
 const loginPage = new LoginPage()
 const dashboardPage = new DashboardPage()
 const myinfoPage = new MyinfoPage
@@ -14,7 +17,7 @@ describe('User Orange HRM Tests', () => {
     loginPage.loginWithUser(userData.userSucceess.username, userData.userSucceess.password)
     dashboardPage.findDashboard()
     myinfoPage.findMyinfoPage()
-    myinfoPage.updateMyinfosFullName('Henrique', 'Galiano', 'Moraes')
+    myinfoPage.updateMyinfosFullName(chance.first(), chance.last(), chance.last())
     myinfoPage.updateMyinfosNationality('Brazilian')
   })
     
