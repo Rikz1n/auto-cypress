8940c0059c598606d930b96d5ae9ea8e7ee7b3f2
Henrique Moraes <rique_moraes@hotmail.com>
2026-02-18 16:58:48 -0300

    Organizando as specs

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -7,18 +7,15 @@ const loginPage = new LoginPage()
 const dashboardPage = new DashboardPage()
 const myinfoPage = new MyinfoPage
 
-describe('Login Orange HRM Tests', () => {
+describe('User Orange HRM Tests', () => {
 
-  it('Login - Success', () => {
+  it('Login - User Info Update - Success', () => {
     loginPage.acessLoginPage()
     loginPage.loginWithUser(userData.userSucceess.username, userData.userSucceess.password)
     dashboardPage.findDashboard()
+    myinfoPage.findMyinfoPage()
+    myinfoPage.updateMyinfosFullName('Henrique', 'Galiano', 'Moraes')
+    myinfoPage.updateMyinfosNationality('Brazilian')
   })
-  
-  it('Login - Fail', () => {
-    loginPage.acessLoginPage()
-    loginPage.loginWithUser(userData.userFail.username, userData.userFail.password)
-    loginPage.loginFailAlert()
-  })
-
+    
 })
